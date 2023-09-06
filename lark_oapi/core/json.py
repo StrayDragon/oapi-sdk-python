e6e2731d180d89af9468689c7db71987ae807f21
straydragon <straydragonl@foxmail.com>
2023-09-06 16:24:30 +0800

    fix: json encode override issus, priority default > cls

--- lark_oapi/core/json.py
+++ lark_oapi/core/json.py
@@ -1,6 +1,6 @@
 import copy
 import datetime
-from json import *
+import json
 from typing import *
 
 from .const import UTF_8
@@ -8,34 +8,22 @@ from .type import T
 
 
 class JSON(object):
-
     @staticmethod
     def marshal(obj: Any, indent=None) -> Optional[str]:
         if obj is None:
             return None
-        return dumps(obj, cls=Encoder, indent=indent, ensure_ascii=False)
+        return json.dumps(
+            obj,
+            indent=indent,
+            ensure_ascii=False,
+            default=custom_json_encode_default,
+        )
 
     @staticmethod
     def unmarshal(json_str: str, clazz: Type[T]) -> T:
-        dict_obj = loads(json_str)
+        dict_obj = json.loads(json_str)
         return clazz(dict_obj)
 
-
-class Encoder(JSONEncoder):
-    def default(self, o: Any) -> Any:
-        if hasattr(o, "__dict__"):
-            return filter_null(copy.deepcopy(vars(o)))
-        if isinstance(o, datetime.datetime):
-            return o.strftime("%Y-%m-%d %H:%M:%S")
-        if isinstance(o, bytes):
-            return str(o, encoding=UTF_8)
-        if isinstance(o, int):
-            return int(o)
-        if isinstance(o, float):
-            return float(o)
-        if isinstance(o, set):
-            return list(o)
-        return super().default(o)
 
 
 def filter_null(d: Dict) -> Dict:
@@ -47,3 +35,21 @@ def filter_null(d: Dict) -> Dict:
                 del d[k]
 
     return d
+
+
+def custom_json_encode_default(o):
+    if hasattr(o, "__dict__"):
+        return filter_null(copy.deepcopy(vars(o)))
+    if isinstance(o, datetime.datetime):
+        return o.strftime("%Y-%m-%d %H:%M:%S")
+    if isinstance(o, bytes):
+        return str(o, encoding=UTF_8)
+    if isinstance(o, int):
+        return int(o)
+    if isinstance(o, float):
+        return float(o)
+    if isinstance(o, set):
+        return list(o)
+    raise TypeError(
+        f"Object of type {o.__class__.__name__} " "is not JSON serializable"
+    )
