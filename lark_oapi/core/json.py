da223b3b03d7931a9018cd1238d6a45f9a6be941
straydragon <straydragonl@foxmail.com>
2023-09-06 16:24:30 +0800

    fix: json encode override issus, priority default > cls

--- lark_oapi/core/json.py
+++ lark_oapi/core/json.py
@@ -1,5 +1,5 @@
 import datetime
-from json import *
+import json
 from typing import *
 
 from .const import UTF_8
@@ -7,31 +7,36 @@ from .type import T
 
 
 class JSON(object):
-
     @staticmethod
     def marshal(obj: Any, indent=None) -> Optional[str]:
         if obj is None:
             return None
-        return dumps(obj, cls=Encoder, indent=indent, ensure_ascii=False)
+        return json.dumps(
+            obj,
+            indent=indent,
+            ensure_ascii=False,
+            default=custom_json_encode_default,
+        )
 
     @staticmethod
     def unmarshal(json_str: str, clazz: Type[T]) -> T:
-        dict_obj = loads(json_str)
+        dict_obj = json.loads(json_str)
         return clazz(dict_obj)
 
 
-class Encoder(JSONEncoder):
-    def default(self, o: Any) -> Any:
-        if hasattr(o, "__dict__"):
-            return vars(o)
-        if isinstance(o, datetime.datetime):
-            return o.strftime("%Y-%m-%d %H:%M:%S")
-        if isinstance(o, bytes):
-            return str(o, encoding=UTF_8)
-        if isinstance(o, int):
-            return int(o)
-        if isinstance(o, float):
-            return float(o)
-        if isinstance(o, set):
-            return list(o)
-        return super().default(o)
+def custom_json_encode_default(o):
+    if hasattr(o, "__dict__"):
+        return vars(o)
+    if isinstance(o, datetime.datetime):
+        return o.strftime("%Y-%m-%d %H:%M:%S")
+    if isinstance(o, bytes):
+        return str(o, encoding=UTF_8)
+    if isinstance(o, int):
+        return int(o)
+    if isinstance(o, float):
+        return float(o)
+    if isinstance(o, set):
+        return list(o)
+    raise TypeError(
+        f"Object of type {o.__class__.__name__} " "is not JSON serializable"
+    )
