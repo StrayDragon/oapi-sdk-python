768f3cffc6356229616ad6671aaa5da98ce889be
straydragon <straydragonl@foxmail.com>
2023-09-07 15:56:07 +0800

    feat: can set retry_config to internal requests

--- lark_oapi/remaintain/shortcut/compact/__init__.py
+++ lark_oapi/remaintain/shortcut/compact/__init__.py
@@ -1,16 +1,26 @@
 import dataclasses
+from typing import Optional
+
+import urllib3
 
 from lark_oapi import Client
 from lark_oapi.remaintain import extra
 from lark_oapi.remaintain.extra import DOMAIN_FEISHU, LEVEL_ERROR
 
+DEFAULT_RETRY_CONFIG = urllib3.Retry(
+    total=3,
+    backoff_factor=0.1,
+    status_forcelist=[502, 503, 504],
+)
 
-@dataclasses.dataclass
+
+@dataclasses.dataclass(repr=False)
 class FeishuOpenAPICompactSettings:
     app_id: str
     app_secret: str
     name: str = "Feishu OAPI"
     log_level: int = LEVEL_ERROR
+    requests_retry_config: Optional[urllib3.Retry] = None
 
     # post init
     remaintain_extra_app_settings: extra.AppSettings = dataclasses.field(init=False)
@@ -23,10 +33,12 @@ class FeishuOpenAPICompactSettings:
             app_secret=self.app_secret,
         )
         self.remaintain_extra_app_settings = app_settings
+        requests_retry_config = self.requests_retry_config
         self.remaintain_extra_config = extra.Config(
             DOMAIN_FEISHU,
             app_settings,
             log_level=self.log_level,
+            requests_retry_config=requests_retry_config,
         )
         self.upstream_client = (
             Client.builder()
@@ -36,5 +48,6 @@ class FeishuOpenAPICompactSettings:
             .app_secret(
                 self.app_secret,
             )
+            .requests_retry_config(requests_retry_config)
             .build()
         )
