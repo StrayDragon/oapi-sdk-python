cafc7a29321f1be9ae8d4a43a4af1a5caf43ae83
straydragon <straydragonl@foxmail.com>
2024-01-04 13:31:41 +0800

    bump: version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", mode="r", encoding="utf-8") as f:
 
 setup(
     name="lark-oapi-shortcut",
-    version="2023.12.8",
+    version="2024.1.4",
     description="Lark OpenAPI SDK for Python",
     long_description=readme,
     long_description_content_type="text/markdown",
